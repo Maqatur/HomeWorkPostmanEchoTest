32029ef29122ec7fd22710bb6c2ff6f1dc49e148
Maqatur <maqaturs@gmail.com>
2024-04-17 22:15:20 +0400

    Test CI

--- src/test/java/ru/netology/tests/PostmanEchoTestEng.java
+++ src/test/java/ru/netology/tests/PostmanEchoTestEng.java
@@ -6,6 +6,20 @@ public class PostmanEchoTestEng {
     @Test
     void postmanEchoTestEng() {
 
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+
     }
 
 }
