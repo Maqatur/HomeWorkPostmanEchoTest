a327c473257bdd28cb4a6e7eb66cc031b28a14ad
Maqatur <maqaturs@gmail.com>
2024-04-17 22:43:01 +0400

    PostmanEcho Test

--- src/test/java/ru/netology/tests/PostmanEchoTestEng.java
+++ src/test/java/ru/netology/tests/PostmanEchoTestEng.java
@@ -2,23 +2,25 @@ package ru.netology.tests;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class PostmanEchoTestEng {
     @Test
     void postmanEchoTestEng() {
+        String text = "PostmanEcho test";
 
         // Given - When - Then
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body(text) // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
-        ;
+                .body("data", org.hamcrest.Matchers.equalTo(text));
 
     }
 
